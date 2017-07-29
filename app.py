c6c89e8beda0c017dc543337203a4e1c86262909
Gluckee <glu9@mail.ru>
2017-07-29 21:16:04 +0300

    rename core to app

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
 from keras.models import load_model
 from flask import Flask, jsonify, request
-import unicodedata, re
-from nltk.stem.snowball import SnowballStemmer
-from nltk.corpus import stopwords
-import cPickle
+import re
+import _pickle as cPickle
 import numpy as np
 app = Flask(__name__)
 
 model = load_model("weights.hdf5")
 
+
+
 with open('note_v.p', 'rb') as f:
     note_v = cPickle.load(f)
-
-with open('title_v.p', 'rb') as f:
-    title_v = cPickle.load(f)
 
 with open('full_categorys.p', 'rb') as f:
     full_categorys = cPickle.load(f)
 
-stemmer = SnowballStemmer('russian')
-stopwords = set(stopwords.words('russian'))
-pattern = re.compile(ur'\b\w\w+\b', re.U)
+pattern = re.compile(r'\b\w\w+\b')
+
 def stem(s):
-    try:
-        s = u''.join([c for c in unicodedata.normalize('NFKD', unicode(s.lower())) if not unicodedata.combining(c)])
-        return u' '.join(map(stemmer.stem, filter(lambda x: x.isalpha() and x not in stopwords, pattern.findall(s))))
-    except:
-        return u''
+    return ' '.join(pattern.findall(s))
 
 def pad_sentences(sentences, padding_word="<PAD/>", sequence_length = 30):
     """
@@ -42,28 +34,31 @@ def pad_sentences(sentences, padding_word="<PAD/>", sequence_length = 30):
     return padded_sentences
 
 
-def predict(title, text):
-    titles = pad_sentences(
-        map(lambda x: filter(None, stem(x).split(' ')), title),
-        sequence_length=16)
+def predict(text):
     notes = pad_sentences(map(lambda x: filter(None, stem(x).split(' ')), text),
-                          sequence_length=128)
-
-    x_t = np.array([[title_v[word] if word in title_v else title_v["<UNK/>"] for
-                     word in sentence]
-                    for sentence in titles])
+                          sequence_length=256)
 
     x_n = np.array([[note_v[word] if word in note_v else note_v["<UNK/>"] for
                      word in sentence]
                     for sentence in notes])
 
-    return full_categorys[model.predict([x_t, x_n]).argmax()]
+    pred_values = {full_categorys[idx]: value for idx, value in enumerate(model.predict([x_n])[0])}
+
+    sorted_values = sorted(pred_values.items(), key = lambda x: x[1], reverse=True)
+    if sorted_values[0][1] > 0.5:
+        return [sorted_values[0][0]]
+    elif sorted_values[0][1] + sorted_values[1][1] > 0.5:
+        return [sorted_values[0][0], sorted_values[1][0]]
+    else:
+        return [sorted_values[0][0], sorted_values[1][0], sorted_values[2][0]]
 
 @app.route('/predict/', methods=['GET'])
 def prediction():
     text = request.args.get('text')
 
-    return jsonify({'doctor_id': predict([u''], [text])})
+    return jsonify({'doctor_ids': predict([text])})
+
+context = ('cert.crt', 'key.key')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=False, ssl_context=context)
